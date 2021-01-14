fc3fac02452e9f0f774bd4577397390fa5209d3c
Branden J Brown <zephyrtronium@gmail.com>
2021-01-14 01:52:20 -0500

    fix one of two Load races

--- syncmap.go
+++ syncmap.go
@@ -1,20 +1,32 @@
 // Package syncmap implements a concurrent read-mostly map, much like sync.Map.
 package syncmap
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 // Map is a concurrent read-mostly map, much like sync.Map with string keys.
 type Map struct {
+	v  atomic.Value // map[string]interface{}
 	mu sync.Mutex
-	v  map[string]interface{}
 }
 
 // Store sets the value at a key.
 func (m *Map) Store(k string, v interface{}) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	if m.v == nil {
-		m.v = make(map[string]interface{})
+	mv, _ := m.v.Load().(map[string]interface{})
+	if mv == nil {
+		mv = make(map[string]interface{})
+		m.v.Store(mv)
 	}
-	m.v[k] = v
+	mv[k] = v
+}
+
+// Load gets the value at a key. ok is false if the key was not in the map.
+func (m *Map) Load(k string) (v interface{}, ok bool) {
+	mv, _ := m.v.Load().(map[string]interface{})
+	v, ok = mv[k]
+	return v, ok
 }
